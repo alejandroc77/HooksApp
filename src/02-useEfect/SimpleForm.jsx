77d61f9325a5a81eb1ed3e2b5b7b7acf51123c9e
Jordy Maza <cabreraalejandro1828@gmail.com>
2022-10-02 14:36:05 -0500

    nuevos elements

--- src/02-useEfect/SimpleForm.jsx
+++ src/02-useEfect/SimpleForm.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "jordy",
+    email: "jamazac@gmail.com",
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value, //propiedades computadas de los objetos !revisar mas a fondo https://www.javascripttutorial.net/es6/javascript-computed-property/
+    });
+  };
   return (
     <>
       <h1>Simple Form</h1>
@@ -8,12 +24,16 @@ export const SimpleForm = () => {
         className="form-control"
         placeholder="Username"
         name="username"
+        value={username}
+        onChange={onInputChange}
       />
       <input
         type="email"
         className="form-control mt-3"
         placeholder="jordymaza@gmail.com"
         name="email"
+        value={email}
+        onChange={onInputChange}
       />
     </>
   );
